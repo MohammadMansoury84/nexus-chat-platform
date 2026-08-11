648ed225b87aaacf88316b514f3d86d8a4a9f266
Mohammad Mansoury <mohammadmansoury1298@gmail.com>
2026-08-11 13:02:58 +0330

    add dependency Injection

--- app/src/main.py
+++ app/src/main.py
@@ -1,6 +1,16 @@
+
+from fastapi import FastAPI
+from src.api.GlobalExceptionHandler.global_exception_handler import GlobalExceptionHandler
+
 def main():
     print("Hello from messenger-f3!")
 
 
+    app=FastAPI()
+    GlobalExceptionHandler(app=app)
+
+
+
 if __name__ == "__main__":
     main()
+
