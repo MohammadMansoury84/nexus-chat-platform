29deafdf878b88913ea1482690560c3f167259ac
Mohammad Mansoury <mohammadmansoury1298@gmail.com>
2026-08-14 06:25:41 +0330

    complete message service

---
+++ app/src/api/schemas/Response/chat_message_response.py
@@ -0,0 +1,12 @@
+from uuid import UUID
+from pydantic import BaseModel, ConfigDict
+
+
+class ChatMessageResponse(BaseModel):
+    model_config = ConfigDict(
+        validate_assignment=True,
+    )
+    sender_id: UUID
+    username: str
+    content: str
+    status: str
--- app/src/api/v1/endpoints/message_router.py
+++ app/src/api/v1/endpoints/message_router.py
@@ -2,6 +2,7 @@ from typing import Annotated
 from uuid import UUID
 
 from fastapi import APIRouter, Depends,status
+from src.api.schemas.Response.chat_message_response import ChatMessageResponse
 from src.api.schemas.Response.response import Response
 from src.application.service.service_Interface.message_service import MessageService
 from src.api.schemas.Request.send_message_request import SendMessageRequest
@@ -46,4 +47,66 @@ def send_message(
             status=message.status
         ),
         message="message send successfully"
+    )
+
+@message_router.get(
+    "/chat/{user2_id}",
+    response_model=Response[list[ChatMessageResponse]],
+    status_code=status.HTTP_200_OK,
+)
+def get_chat(
+    user2_id: UUID,
+    current_user_id: Annotated[
+        UUID,
+        Depends(get_current_user_id),
+    ],
+    message_service: Annotated[
+        MessageService,
+        Depends(get_message_service),
+    ],
+) -> Response[list[ChatMessageResponse]]:
+
+    messages = message_service.get_chat(
+        user1_id=current_user_id,
+        user2_id=user2_id,
+    )
+
+    return Response[list[ChatMessageResponse]](
+        data=[
+            ChatMessageResponse(
+                sender_id=message.sender_id,
+                username=message.username,
+                content=message.content,
+                status=message.status,
+            )
+            for message in messages
+        ]
+    )
+
+
+@message_router.delete(
+    "/chat/{user2_id}",
+    response_model=Response[bool],
+    status_code=status.HTTP_200_OK,
+)
+def delete_private_chat_history(
+    user2_id: UUID,
+    current_user_id: Annotated[
+        UUID,
+        Depends(get_current_user_id),
+    ],
+    message_service: Annotated[
+        MessageService,
+        Depends(get_message_service),
+    ],
+) -> Response[bool]:
+
+    deleted = message_service.delete_private_chat_history(
+        user1_id=current_user_id,
+        user2_id=user2_id,
+    )
+
+    return Response[bool](
+        data=deleted,
+        message="Chat history deleted successfully",
     )
