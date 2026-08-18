cb0455ae05d65bd001f499e2b8c8d8617051c60c
Mohammad Mansoury <mohammadmansoury1298@gmail.com>
2026-08-18 10:04:22 +0330

    Refactor and update various components in the application
    - Updated the `Setting.py` configuration to correct variable names and added `max_overflow` parameter.
    - Modified `detabase_set_up.py` to utilize the new settings for database connection.
    - Fixed spelling errors in variable names across multiple files.
    - Updated dependencies in `pyproject.toml` to include `psycopg[binary]` for PostgreSQL support.
    - Regenerated `.pyc` files for all modified Python files to reflect the latest changes.

--- app/src/core/config/Setting.py
+++ app/src/core/config/Setting.py
@@ -7,10 +7,11 @@ class Setting(BaseSettings):
     jwt_secret_key: SecretStr
     jwt_algorithm: str
     access_token_expire_minutes: int
-    darabase_url: str
+    database_url: str
     pool_timeout: int
     pool_size: int
     echo: bool
-    isolated: str
+    isolated_level: str
+    max_overflow: int
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
--- app/src/infrastructure/database/detabase_set_up.py
+++ app/src/infrastructure/database/detabase_set_up.py
@@ -1,3 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine
+from src.core.config.Setting import Setting
 
-async_engine = create_async_engine()
+setting = Setting()
+
+async_engine = create_async_engine(
+    url=setting.database_url,
+    echo=setting.echo,
+    pool_timeout=setting.pool_timeout,
+    pool_size=setting.pool_size,
+    isolated_level=setting.isolated_level,
+)
